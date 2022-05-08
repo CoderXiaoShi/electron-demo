443787c1587ead1067b9dc4c72801b3a533a35ee
石兴龙 <1003865572@qq.com>
2022-05-09 00:33:12 +0800

    update
    update

---
+++ demo-02/index.js
@@ -0,0 +1,19 @@
+var { app, BrowserWindow }  = require('electron')
+
+let win = null
+
+app.on('ready', () => {
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  win.loadFile('./index.html');
+  win.on('closed', () => {
+    win = null
+  })
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
