556d44d1ec37b28e11553fcdea5865d723483a0a
石兴龙 <1003865572@qq.com>
2022-05-09 23:54:51 +0800

    update

--- demo-02/index.js
+++ demo-02/index.js
@@ -1,17 +1,36 @@
-var { app, BrowserWindow }  = require('electron')
+var { app, BrowserWindow, ipcMain, dialog }  = require('electron')
+const path = require('path')
 
 let win = null
+
+async function handleFileOpen(e, val) {
+  console.log('handleFileOpen', val)
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (canceled) {
+    return
+  }
+  return filePaths[0]
+}
 
 app.on('ready', () => {
   win = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      preload: path.join(__dirname, './preload.js')
     }
   })
   win.loadFile('./index.html');
   win.on('closed', () => {
-    win = null
+    win = null;
   })
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender
+    const win = BrowserWindow.fromWebContents(webContents)
+    console.log(title)
+    win.setTitle(`--${title}--`)
+
+  })
+  ipcMain.handle('dialog:openFIle', handleFileOpen)
 })
 
 app.on('window-all-closed', () => {
